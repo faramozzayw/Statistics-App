b2dd707a9cb4d8fd02e19d3acf5fc559594653e9
faramo_zayw <faramo.zayw@gmail.com>
2019-04-08 20:01:10 +0300

    Updates

---
+++ precache-manifest.c21e05c409fa9add9500fe22bccd9a27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e50d31f651fe037f8650",
+    "url": "/Statistics-App/static/css/main.77af5489.chunk.css"
+  },
+  {
+    "revision": "e50d31f651fe037f8650",
+    "url": "/Statistics-App/static/js/main.cd4f5324.chunk.js"
+  },
+  {
+    "revision": "290c14b76f065ff0c87e",
+    "url": "/Statistics-App/static/js/runtime~main.f6795ac4.js"
+  },
+  {
+    "revision": "591f0603297018de3042",
+    "url": "/Statistics-App/static/js/2.81606434.chunk.js"
+  },
+  {
+    "revision": "7d2be74856302e0e24444b1199301717",
+    "url": "/Statistics-App/index.html"
+  }
+];
--- src/function/ParseToArray.js
+++
@@ -1,9 +0,0 @@
-const ParseToArray = string => {
-	let array = string.split(',');
-	let result = [];
-	for(let i = 0; i < array.length; i++)
-		result.push(Number(array[i]));
-	return result;
-}
-
-export default ParseToArray;
